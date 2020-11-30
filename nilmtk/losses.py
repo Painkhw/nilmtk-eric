92fa8a4aaa014cbdd55d807f1dbaa6fcd370fa58
root <root@debian.linuxeric.edu>
2020-11-30 09:51:39 +0800

    add some loss function

--- nilmtk/losses.py
+++ nilmtk/losses.py
@@ -26,12 +26,12 @@ def accuracy(app_gt, app_pred):
     return accuracy_score(gt_temp, pred_temp)
 
 def precision(app_gt, app_pred):
-    threshold = 10
+    threshold = 10 
     gt_temp = np.array(app_gt)
     gt_temp = np.where(gt_temp<threshold,0,1)
     pred_temp = np.array(app_pred)
     pred_temp = np.where(pred_temp<threshold,0,1)
-
+    
     return precision_score(gt_temp, pred_temp)
 
 def recall(app_gt, app_pred):
@@ -47,7 +47,7 @@ def relative_error(app_gt,app_pred):
     constant = 1
     numerator = np.abs(app_gt - app_pred)
     denominator = constant + app_pred
-    
+
     return np.mean(numerator/denominator)
 
 def r2score(app_gt,app_pred):
