c535f031a2e3bac2bab21bc62c8dac893e870496
root <root@debian.linuxeric.edu>
2020-12-10 09:34:38 +0800

    modify plot

--- result/plot.py
+++ result/plot.py
@@ -14,7 +14,6 @@ ax.yaxis.set_major_formatter(format)
 ax1.yaxis.set_major_formatter(format)
 df['c'].plot(ax=ax1,figsize=(9,6),style=['g-'])
 ax1.legend(('helloworld',),loc='upper center')
-df[['aaa','bbb']].plot(kind='bar',grid=True,ax=ax,figsize=(9,7)) #柱状图应该在折线图之后画，防止因为折线图所需要的坐标轴面积更小而让柱状图结果显示不全
- 
+df[['aaa','bbb']].plot(kind='bar',grid=True,ax=ax,figsize=(9,7))
 ax.plot(df.index,[1,2,3,4])
 ax.legend(('new','aaa','bbb'))
